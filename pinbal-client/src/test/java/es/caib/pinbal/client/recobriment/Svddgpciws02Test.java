9f23d30d2715f7f0013604080da3196e297fd88b
josepg-limit <josepg@limit.es>
2021-01-26 16:17:38 +0100

    #125 Afegir servei de verificació de dades d'entitat a la llibreria que facilita les consultes SCSP Agegits tests unitaris

---
+++ pinbal-client/src/test/java/es/caib/pinbal/client/recobriment/Svddgpciws02Test.java
@@ -0,0 +1,90 @@
+package es.caib.pinbal.client.recobriment;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.sun.jersey.api.client.ClientHandlerException;
+import com.sun.jersey.api.client.UniformInterfaceException;
+import es.caib.pinbal.client.recobriment.model.ScspFuncionario;
+import es.caib.pinbal.client.recobriment.model.ScspJustificante;
+import es.caib.pinbal.client.recobriment.model.ScspRespuesta;
+import es.caib.pinbal.client.recobriment.model.ScspSolicitante.ScspConsentimiento;
+import es.caib.pinbal.client.recobriment.model.ScspTitular;
+import es.caib.pinbal.client.recobriment.model.ScspTitular.ScspTipoDocumentacion;
+import es.caib.pinbal.client.recobriment.svddgpciws02.ClientSvddgpciws02;
+import es.caib.pinbal.client.recobriment.svddgpciws02.ClientSvddgpciws02.SolicitudSvddgpciws02;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Test del client genèric del recobriment.
+ * 
+ * @author Limit Tecnologies <limit@limit.es>
+ */
+public class Svddgpciws02Test {
+
+	private static final String URL_BASE = "http://localhost:8080/pinbal";
+	private static final String USUARI = "user";
+	private static final String CONTRASENYA = "passwd";
+	private static final String SERVEI_SCSP = "pruebaPMI";
+	private static final String PETICION_SCSP_ID = "PBL0000000001292";
+	private static final boolean IS_JBOSS = false;
+
+	private ClientSvddgpciws02 client = new ClientSvddgpciws02(URL_BASE, USUARI, CONTRASENYA, !IS_JBOSS, null, null);
+
+	@Test
+	public void peticionSincrona() throws UniformInterfaceException, ClientHandlerException, IOException {
+		SolicitudSvddgpciws02 solicitud = new SolicitudSvddgpciws02();
+		solicitud.setIdentificadorSolicitante("B07167448");
+		solicitud.setCodigoProcedimiento("ProvaConcepte");
+		solicitud.setUnidadTramitadora("Departament de test");
+		solicitud.setFinalidad("Test peticionSincrona");
+		solicitud.setConsentimiento(ScspConsentimiento.Si);
+		ScspFuncionario funcionario = new ScspFuncionario();
+		funcionario.setNifFuncionario("00000000T");
+		funcionario.setNombreCompletoFuncionario("Funcionari CAIB");
+		solicitud.setFuncionario(funcionario);
+		ScspTitular titular = new ScspTitular();
+		titular.setTipoDocumentacion(ScspTipoDocumentacion.NIF);
+		titular.setDocumentacion("12345678Z");
+		solicitud.setTitular(titular);
+		ScspRespuesta respuesta = client.peticionSincrona(
+				SERVEI_SCSP,
+				Arrays.asList(solicitud));
+		assertNotNull(respuesta);
+		System.out.println("-> peticionSincrona(" + SERVEI_SCSP + ") = " + objectToJsonString(respuesta));
+	}
+
+	@Test
+	public void getRespuesta() throws IOException {
+		ScspRespuesta respuesta = client.getRespuesta(PETICION_SCSP_ID);
+		assertNotNull(respuesta);
+		System.out.println("-> getRespuesta(" + PETICION_SCSP_ID + ") = " + objectToJsonString(respuesta));
+	}
+
+	@Test
+	public void getJustificante() throws IOException {
+		ScspJustificante justificante = client.getJustificante(PETICION_SCSP_ID);
+		assertNotNull(justificante);
+		System.out.println("-> getJustificante");
+		System.out.println("\tnom: " + justificante.getNom());
+		System.out.println("\tcontentType: " + justificante.getContentType());
+		System.out.println("\tcontingut: " + justificante.getContingut());
+	}
+
+	private String objectToJsonString(Object obj) throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+		mapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
+		mapper.setSerializationInclusion(Include.NON_NULL);
+		mapper.enable(SerializationFeature.INDENT_OUTPUT);
+		return mapper.writeValueAsString(obj);
+	}
+
+}
---
+++ pinbal-client/src/test/java/es/caib/pinbal/client/recobriment/Svddgpviws02Test.java
@@ -0,0 +1,90 @@
+package es.caib.pinbal.client.recobriment;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.sun.jersey.api.client.ClientHandlerException;
+import com.sun.jersey.api.client.UniformInterfaceException;
+import es.caib.pinbal.client.recobriment.model.ScspFuncionario;
+import es.caib.pinbal.client.recobriment.model.ScspJustificante;
+import es.caib.pinbal.client.recobriment.model.ScspRespuesta;
+import es.caib.pinbal.client.recobriment.model.ScspSolicitante.ScspConsentimiento;
+import es.caib.pinbal.client.recobriment.model.ScspTitular;
+import es.caib.pinbal.client.recobriment.model.ScspTitular.ScspTipoDocumentacion;
+import es.caib.pinbal.client.recobriment.svddgpviws02.ClientSvddgpviws02.SolicitudSvddgpviws02;
+import es.caib.pinbal.client.recobriment.svddgpviws02.ClientSvddgpviws02;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Test del client genèric del recobriment.
+ * 
+ * @author Limit Tecnologies <limit@limit.es>
+ */
+public class Svddgpviws02Test {
+
+	private static final String URL_BASE = "http://localhost:8080/pinbal";
+	private static final String USUARI = "user";
+	private static final String CONTRASENYA = "passwd";
+	private static final String SERVEI_SCSP = "pruebaPMI";
+	private static final String PETICION_SCSP_ID = "PBL0000000001292";
+	private static final boolean IS_JBOSS = false;
+
+	private ClientSvddgpviws02 client = new ClientSvddgpviws02(URL_BASE, USUARI, CONTRASENYA, !IS_JBOSS, null, null);
+
+	@Test
+	public void peticionSincrona() throws UniformInterfaceException, ClientHandlerException, IOException {
+		SolicitudSvddgpviws02 solicitud = new SolicitudSvddgpviws02();
+		solicitud.setIdentificadorSolicitante("B07167448");
+		solicitud.setCodigoProcedimiento("ProvaConcepte");
+		solicitud.setUnidadTramitadora("Departament de test");
+		solicitud.setFinalidad("Test peticionSincrona");
+		solicitud.setConsentimiento(ScspConsentimiento.Si);
+		ScspFuncionario funcionario = new ScspFuncionario();
+		funcionario.setNifFuncionario("00000000T");
+		funcionario.setNombreCompletoFuncionario("Funcionari CAIB");
+		solicitud.setFuncionario(funcionario);
+		ScspTitular titular = new ScspTitular();
+		titular.setTipoDocumentacion(ScspTipoDocumentacion.NIF);
+		titular.setDocumentacion("12345678Z");
+		solicitud.setTitular(titular);
+		ScspRespuesta respuesta = client.peticionSincrona(
+				SERVEI_SCSP,
+				Arrays.asList(solicitud));
+		assertNotNull(respuesta);
+		System.out.println("-> peticionSincrona(" + SERVEI_SCSP + ") = " + objectToJsonString(respuesta));
+	}
+
+	@Test
+	public void getRespuesta() throws IOException {
+		ScspRespuesta respuesta = client.getRespuesta(PETICION_SCSP_ID);
+		assertNotNull(respuesta);
+		System.out.println("-> getRespuesta(" + PETICION_SCSP_ID + ") = " + objectToJsonString(respuesta));
+	}
+
+	@Test
+	public void getJustificante() throws IOException {
+		ScspJustificante justificante = client.getJustificante(PETICION_SCSP_ID);
+		assertNotNull(justificante);
+		System.out.println("-> getJustificante");
+		System.out.println("\tnom: " + justificante.getNom());
+		System.out.println("\tcontentType: " + justificante.getContentType());
+		System.out.println("\tcontingut: " + justificante.getContingut());
+	}
+
+	private String objectToJsonString(Object obj) throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+		mapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
+		mapper.setSerializationInclusion(Include.NON_NULL);
+		mapper.enable(SerializationFeature.INDENT_OUTPUT);
+		return mapper.writeValueAsString(obj);
+	}
+
+}
