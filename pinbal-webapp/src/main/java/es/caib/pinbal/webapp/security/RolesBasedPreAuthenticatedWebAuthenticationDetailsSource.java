8fbc2631a199126aaebda83fa59c8744bc3ad809
Josep Gayà <josepg@limit.es>
2021-06-01 11:09:04 +0200

    #149 Poder consultar els rols SEYCON a la pantalla de preferències d'usuari

--- pinbal-webapp/src/main/java/es/caib/pinbal/webapp/security/RolesBasedPreAuthenticatedWebAuthenticationDetailsSource.java
+++ pinbal-webapp/src/main/java/es/caib/pinbal/webapp/security/RolesBasedPreAuthenticatedWebAuthenticationDetailsSource.java
@@ -3,12 +3,15 @@
  */
 package es.caib.pinbal.webapp.security;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.authority.mapping.MappableAttributesRetriever;
 import org.springframework.security.web.authentication.preauth.j2ee.J2eeBasedPreAuthenticatedWebAuthenticationDetailsSource;
 
@@ -28,18 +31,24 @@ public class RolesBasedPreAuthenticatedWebAuthenticationDetailsSource extends J2
 	@Override
 	protected Collection<String> getUserRoles(HttpServletRequest request) {
 		Set<String> roles = mappableAttributesRetriever.getMappableAttributes();
-        Set<String> j2eeUserRolesList = new HashSet<String>();
-        for (String role: roles) {
-            if (request.isUserInRole(role)) {
-                j2eeUserRolesList.add(role);
-            }
-        }
-        return j2eeUserRolesList;
-    }
+		Set<String> j2eeUserRolesList = new HashSet<String>();
+		for (String role: roles) {
+			if (request.isUserInRole(role)) {
+				j2eeUserRolesList.add(role);
+			}
+		}
+		LOGGER.debug(
+				"Rols SEYCON de l'usuari " + request.getUserPrincipal().getName() + ": " +
+				Arrays.toString(j2eeUserRolesList.toArray(new String[j2eeUserRolesList.size()])));
+		return j2eeUserRolesList;
+	}
+
 	@Override
 	public void setMappableRolesRetriever(MappableAttributesRetriever mappableAttributesRetriever) {
-        this.mappableAttributesRetriever = mappableAttributesRetriever;
-        this.j2eeMappableRoles = new HashSet<String>();
-    }
+		this.mappableAttributesRetriever = mappableAttributesRetriever;
+		this.j2eeMappableRoles = new HashSet<String>();
+	}
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(RolesBasedPreAuthenticatedWebAuthenticationDetailsSource.class);
 
 }
