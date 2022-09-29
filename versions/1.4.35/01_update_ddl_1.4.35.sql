5e6378eb8a5b6f36dac5fa43c6a037946d1defcd
josepg-limit <josepg@limit.es>
2022-09-29 11:26:41 +0200

    Preparar v.1.4.35

---
+++ versions/1.4.35/01_update_ddl_1.4.35.sql
@@ -0,0 +1,2 @@
+-- #243
+ALTER TABLE pbl_servei_config ADD use_auto_classe NUMBER(1) DEFAULT '1';
---
+++ versions/1.4.35/02_update_dml_1.4.35.sql
@@ -0,0 +1,21 @@
+-- #242
+UPDATE pbl_procediment SET valor_camp_clasetramite = 0 WHERE valor_camp_clasetramite = 19;
+UPDATE pbl_procediment SET valor_camp_clasetramite = 19 WHERE valor_camp_clasetramite = 2;
+UPDATE pbl_procediment SET valor_camp_clasetramite = 20 WHERE valor_camp_clasetramite = 3;
+UPDATE pbl_procediment SET valor_camp_clasetramite = 21 WHERE valor_camp_clasetramite = 4;
+UPDATE pbl_procediment SET valor_camp_clasetramite = 22 WHERE valor_camp_clasetramite = 5;
+UPDATE pbl_procediment SET valor_camp_clasetramite = 23 WHERE valor_camp_clasetramite = 6;
+UPDATE pbl_procediment SET valor_camp_clasetramite = 24 WHERE valor_camp_clasetramite = 7;
+UPDATE pbl_procediment SET valor_camp_clasetramite = 25 WHERE valor_camp_clasetramite = 8;
+UPDATE pbl_procediment SET valor_camp_clasetramite = 26 WHERE valor_camp_clasetramite = 9;
+UPDATE pbl_procediment SET valor_camp_clasetramite = 27 WHERE valor_camp_clasetramite = 10;
+UPDATE pbl_procediment SET valor_camp_clasetramite = 28 WHERE valor_camp_clasetramite = 11;
+UPDATE pbl_procediment SET valor_camp_clasetramite = 29 WHERE valor_camp_clasetramite = 12;
+UPDATE pbl_procediment SET valor_camp_clasetramite = 30 WHERE valor_camp_clasetramite = 14;
+UPDATE pbl_procediment SET valor_camp_clasetramite = 31 WHERE valor_camp_clasetramite = 15;
+UPDATE pbl_procediment SET valor_camp_clasetramite = 32 WHERE valor_camp_clasetramite = 16;
+UPDATE pbl_procediment SET valor_camp_clasetramite = 33 WHERE valor_camp_clasetramite = 18;
+UPDATE pbl_procediment SET valor_camp_clasetramite = 34 WHERE valor_camp_clasetramite = 1;
+UPDATE pbl_procediment SET valor_camp_clasetramite = 2 WHERE valor_camp_clasetramite = 13;
+UPDATE pbl_procediment SET valor_camp_clasetramite = 3 WHERE valor_camp_clasetramite = 0;
+UPDATE pbl_procediment SET valor_camp_clasetramite = 14 WHERE valor_camp_clasetramite = 17;
